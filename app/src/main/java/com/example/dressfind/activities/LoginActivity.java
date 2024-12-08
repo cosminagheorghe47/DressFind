1bf6665e7a7a1782daba3f575d82f0e8ad3feaee
Elena Georgescu <101599503+elenaag23@users.noreply.github.com>
2024-12-08 19:53:22 +0200

    Finished register functionality

--- app/src/main/java/com/example/dressfind/activities/LoginActivity.java
+++ app/src/main/java/com/example/dressfind/activities/LoginActivity.java
@@ -6,9 +6,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.dressfind.R;
@@ -20,6 +20,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText emailView;
     private EditText passwordView;
     private Button loginButton;
+    private TextView registerLink;
 
 
     @Override
@@ -32,33 +33,33 @@ public class LoginActivity extends AppCompatActivity {
         emailView = findViewById(R.id.email);
         passwordView = findViewById(R.id.password);
         loginButton = findViewById(R.id.loginButton);
+        registerLink = findViewById(R.id.register_link);
+
+        registerLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Get user input
                 String email = emailView.getText().toString().trim();
                 String password = passwordView.getText().toString().trim();
 
-                // Validate inputs
                 if (email.isEmpty() || password.isEmpty()) {
                     Toast.makeText(LoginActivity.this, "Please enter both email and password", Toast.LENGTH_SHORT).show();
                 } else {
-                    // For now, just log the inputs
-                    Log.i("LoginActivity", "Email: " + email);
-                    Log.i("LoginActivity", "Password: " + password);
-
                     auth.signInWithEmailAndPassword(email, password)
                             .addOnCompleteListener(LoginActivity.this, task -> {
                                 if (task.isSuccessful()) {
-                                    // Sign-in success
                                     FirebaseUser user = auth.getCurrentUser();
                                     Log.i("LoginActivity", "User signed in: " + user.getEmail());
                                     startActivity(new Intent(LoginActivity.this, MainPageActivity.class));
-                                    finish();  // Close the login activity
                                 } else {
-                                    // If sign-in fails
-                                    Toast.makeText(LoginActivity.this, "Authentication failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                    Log.e("LoginActivity", "User login failed: " + task.getException().getMessage());
+                                    Toast.makeText(LoginActivity.this, "Authentication failed" , Toast.LENGTH_SHORT).show();
                                 }
                             });
                 }
--- app/src/main/java/com/example/dressfind/activities/RegisterActivity.java
+++ app/src/main/java/com/example/dressfind/activities/RegisterActivity.java
@@ -1,17 +1,115 @@
 package com.example.dressfind.activities;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.dressfind.R;
+import com.example.dressfind.models.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Date;
 
 public class RegisterActivity extends AppCompatActivity {
+    private EditText emailView;
+    private EditText firstNameView;
+    private EditText lastNameView;
+    private EditText passwordView;
+    private EditText confirmPasswordView;
+    private Button registerButton;
+    private TextView loginLink;
+    private FirebaseFirestore firestore;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
+
+        emailView = findViewById(R.id.email);
+        firstNameView = findViewById(R.id.first_name);
+        lastNameView = findViewById(R.id.last_name);
+        passwordView = findViewById(R.id.password);
+        confirmPasswordView = findViewById(R.id.confirm_password);
+        registerButton = findViewById(R.id.register_button);
+        loginLink = findViewById(R.id.login_link);
+
+        loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailView.getText().toString().trim();
+                String password = passwordView.getText().toString().trim();
+                String confirmPassword = confirmPasswordView.getText().toString().trim();
+                String firstName = firstNameView.getText().toString().trim();
+                String lastName = lastNameView.getText().toString().trim();
+
+
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(RegisterActivity.this, "Enter both email and password", Toast.LENGTH_SHORT).show();
+                } else if (password.length() < 6) {
+                    Toast.makeText(RegisterActivity.this, "Password must be at least 6 characters", Toast.LENGTH_SHORT).show();
+                } else if (!password.equals(confirmPassword)) {
+                Toast.makeText(RegisterActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                }  else {
+                    auth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(task -> {
+                                if (task.isSuccessful()) {
+                                    FirebaseUser user = auth.getCurrentUser();
+
+                                    Log.i("RegisterActivity", "Register task is successful");
+                                    String userId = user.getUid();
+
+                                    User userToSave = new User();
+                                    userToSave.setUserId(userId);
+                                    userToSave.setEmail(email);
+                                    userToSave.setFirstName(firstName);
+                                    userToSave.setLastName(lastName);
+
+                                    Date date = new Date();
+                                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                                        userToSave.setRegistrationDate(date);
+                                    }
+
+                                    Log.i("RegisterActivity", "user object: " + userToSave);
+
+                                    firestore.collection("user").document(userId)
+                                            .set(userToSave)
+                                            .addOnSuccessListener(aVoid -> {
+                                                Log.i("RegisterActivity", "User registered: " + userToSave);
+                                                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                                            })
+                                            .addOnFailureListener(e -> {
+                                                Log.e("RegisterActivity",  "Failed to add user to db: " + e.getMessage());
+                                                Toast.makeText(RegisterActivity.this, "Failed to save user: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                            });
+                                } else {
+                                    Log.e("RegisterActivity", "Register task was not successful: " + task.getException().getMessage());
+                                    Toast.makeText(RegisterActivity.this, "Registration failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                }
+            }
+        });
 
     }
 }
